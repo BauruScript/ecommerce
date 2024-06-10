f794d2f4eed757b2c7c135f5b594442da1778da3
Pedro <106351079+BauruScript@users.noreply.github.com>
2024-06-10 16:41:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.querySelector('.navbar');
+const product = document.querySelector('.pro-container');
+
+let mainImg = document.getElementById("main-img");
+let smallImg = document.getElementsByClassName("small-img");
 
 if(bar) {
     bar.addEventListener('click', () => {
@@ -12,4 +16,21 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+}
+
+
+const updateMainImg = function(e) {
+    let tempSrc = mainImg.src;
+    mainImg.src = smallImg[e].src;
+    smallImg[e].src = tempSrc;
+}
+
+for(let i = 0; i < smallImg.length; i++) {
+    smallImg[i].onclick = function() {
+        updateMainImg(i);
+    }
+}
+
+product.onclick = function() {
+    window.location.href = "sproduct.html"
 }
